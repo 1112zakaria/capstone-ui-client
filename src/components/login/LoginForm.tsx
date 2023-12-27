064cc962dabdfbceb16ecf42fff3b4d2e8cad5de
Zakaria Ismail <1112zakaria@gmail.com>
2023-12-26 22:39:16 -0500

    ICUI-14: Added state management for username and password form
    **Problem:**
    
    **Fix:**
    
    **Testing:**

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,12 +1,27 @@
 import {Button, FormControl, FormHelperText, TextField} from "@mui/material";
+import React, {useState, useEffect} from "react";
 
 function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    console.log("Username: " + username + " Password: " + password);
+  })
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setPassword(e.target.value);
+  }
 
   return (
     <div>
       <FormControl>
-        <TextField label="Username" placeholder="Enter username" fullWidth required/>
-        <TextField label="Password" placeholder="Enter password" type="password" fullWidth required/>
+        <TextField label="Username" placeholder="Enter username" onChange={handleUsernameChange} fullWidth required/>
+        <TextField label="Password" placeholder="Enter password" type="password" onChange={handlePasswordChange} fullWidth required/>
         <Button variant="contained">Sign In</Button>
         <FormHelperText>We'll never share your credentials.</FormHelperText>
       </FormControl>
