e958c86ff1bc7fffa47b1859141bd2ce77f5b4dc
Zakaria Ismail <1112zakaria@gmail.com>
2024-01-02 18:13:42 -0500

    ICUI-21: added a litle green logout button on the right
    **Problem:**
    
    **Fix:**
    somehow it got on the right side, don't know how but its neat
    
    **Testing:**

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,39 +2,47 @@ import {useNavigate} from "react-router-dom";
 import {AppBar, Box, Button, IconButton, Toolbar} from "@mui/material";
 import {SatelliteAlt} from "@mui/icons-material";
 import routes from "./routes";
+import {MouseEvent} from "react";
 
 const Navbar = () => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleNavbarClick = (e: React.MouseEvent, route: string) => {
-        navigate(route);
-    }
+  const handleNavbarClick = (e: React.MouseEvent, route: string) => {
+    navigate(route);
+  }
 
-    return (
-        <nav className="nav">
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton size="large">
-                        <SatelliteAlt />
-                    </IconButton>
-                    <Box sx={ {flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+  const handleLogoutClick = (e: React.MouseEvent) => {
 
-                            {routes.map((route) => (
-                                <Button
-                                    sx={ {my: 2, color: 'white', display: 'block'} }
-                                    onClick={(e) => handleNavbarClick(e, route.url)}
-                                >
-                                    {route.name}
-                                </Button>
-                            ))}
+  }
 
-                    </Box>
-                </Toolbar>
-            </AppBar>
+  return (
+    <AppBar position="static" component="nav">
+      <Toolbar>
+        <IconButton size="large">
+          <SatelliteAlt/>
+        </IconButton>
+        <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
 
-        </nav>
-    );
+          {routes.map((route) => (
+            <Button
+              sx={{my: 2, color: 'white', display: 'block'}}
+              onClick={(e: React.MouseEvent) => handleNavbarClick(e, route.url)}
+            >
+              {route.name}
+            </Button>
+          ))}
+          {/*  if user is signed in, render a logout button here...
+                  but also, if the user is signed in, don't render the register/sign-in navbar buttons
+                  and if the user is not signed in, don't render the other options? */}
+        </Box>
+        <Box>
+          <Button variant="contained" color="success"
+                  onClick={handleLogoutClick}>Logout</Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
